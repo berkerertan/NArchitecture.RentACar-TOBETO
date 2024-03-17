d1fa32d51e5f346aa3dc556abaa089ffe779f181
Berker ERTAN <berkerertan@hotmail.com>
2024-03-18 01:09:32 +0300

    NewController Methods, Redis Connection Added.

--- src/webProjects/WebAPI/Program.cs
+++ src/webProjects/WebAPI/Program.cs
@@ -1,38 +1,31 @@
-using Application;
 using Persistence;
-namespace WebAPI
+using Application;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddApplicationServices();
+
+builder.Services.AddStackExchangeRedisCache(opt => opt.Configuration = "localhost:6379");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            builder.Services.AddPersistenceServices(builder.Configuration);
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddApplicationServices();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-                //app.ConfigureCustomExceptionMiddleware();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //app.ConfigureCustomExceptionMiddleware();
 }
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
