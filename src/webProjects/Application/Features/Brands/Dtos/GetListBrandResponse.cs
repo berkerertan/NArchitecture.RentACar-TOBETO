af260f69be5b386a4e8e37b127fbb41ab4e67b67
Berker ERTAN <berkerertan@hotmail.com>
2024-03-18 01:10:33 +0300

    Entity Updated

---
+++ src/webProjects/Application/Features/Brands/Dtos/GetListBrandResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.Brands.Dtos
+{
+    public class GetListBrandResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
--- src/webProjects/Application/Features/Brands/Profiles/MappingProfiles.cs
+++ src/webProjects/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -2,7 +2,9 @@
 using Application.Features.Brands.Commands.Delete;
 using Application.Features.Brands.Commands.Update;
 using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Models;
 using AutoMapper;
+using Core.Persistence.Paging;
 using Domain.Entities;
 
 namespace Application.Features.Brands.Profiles
@@ -18,6 +20,8 @@ namespace Application.Features.Brands.Profiles
             CreateMap<Brand,UpdateBrandCommand>().ReverseMap();
             CreateMap<Brand,UpdatedBrandResponse>().ReverseMap();
             CreateMap<Brand,GetByIdBrandResponse>().ReverseMap();
+            CreateMap<Brand,GetListBrandResponse>().ReverseMap();
+            CreateMap<IPaginate<Brand>, BrandListModel>().ReverseMap();
 
         }
     }
