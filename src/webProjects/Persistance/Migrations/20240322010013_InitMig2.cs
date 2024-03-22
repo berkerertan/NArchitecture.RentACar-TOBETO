b9ec11a9e719dc57a4cc31b1ac2447cac7b64e93
Berker ERTAN <berkerertan@hotmail.com>
2024-03-22 04:08:07 +0300

    Migration recreated

--- src/webProjects/Persistance/Migrations/20240322010013_InitMig2.cs
+++ src/webProjects/Persistance/Migrations/20240322010013_InitMig2.cs
@@ -5,7 +5,7 @@
 namespace Persistence.Migrations
 {
     /// <inheritdoc />
-    public partial class InitMig5 : Migration
+    public partial class InitMig2 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
